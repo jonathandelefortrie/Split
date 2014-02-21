70c79b366702023f2f79fa8eb9bfef51a058eeff
Jonathan Delefortrie <jonathandelefortrie@gmail.com>
2014-02-21 11:06:42 -0500

    pixel video canvas

--- main.js
+++ main.js
@@ -1,68 +1,89 @@
-var _width = window.innerWidth;
-var _height = window.innerHeight;
+var imageData, constraints = { video: true, audio: false };
+
+var URL = window.URL ||
+    window.webkitURL ||
+        window.msURL ||
+           window.oURL;
+
+navigator.getUserMedia = navigator.getUserMedia ||
+                   navigator.webkitGetUserMedia ||
+                      navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia;
+
+var mouseX = 0 * 0.5;
+var mouseY = 0 * 0.5;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
 var container = document.getElementById("container");
-container.width = _width;
-container.height = _height;
+container.style.width = 320 + 'px';
+container.style.height = 240 + 'px';
+container.style.backgroundColor= '#CCCCCC';
 
 var cvs = document.createElement("canvas");
 var ctx = cvs.getContext('2d');
 container.appendChild(cvs);
-cvs.width = container.width;
-cvs.height = container.height;
+cvs.width = 320;
+cvs.height = 240;
 
+// Origin pixel
+var cvs2 = document.createElement("canvas");
+var ctx2 = cvs2.getContext('2d');
+cvs2.width = 320;
+cvs2.height = 240;
+
+// Origin source
+var vid = document.createElement('video');
+//document.body.appendChild(vid);
+vid.width = 320;
+vid.height = 240;
 
 (function Main(){
-
-	base = _width/2;
-
-	mouseX = 0 * 0.5;
-	mouseY = 0 * 0.5;
 
 	arrayChildren = new Array();
 
 	ClassCircle = new Circle();
-	FirstChild = new ClassCircle.circle({
-
-		p: { x: _width/2, y: _height/2 },
-		s: _width/4
-
-	});
-
+	FirstChild = new ClassCircle.circle({p: { x: 320/2, y: 240/2 },s: 240/2});
 	arrayChildren.push(FirstChild);
 
-	loop = function() {
+	draw = function() {
 
-		ctx.clearRect(0,0,_width,_height);
+        setTimeout(function() {
 
-		for (i in arrayChildren) {
-			arrayChildren[i].draw();
+			ctx.clearRect(0,0,320, 240);
+			ctx2.clearRect(0,0,320, 240);
+			ctx2.drawImage(vid, 0, 0, 320, 240);
 
-			distance = Math.sqrt(Math.pow(mouseX-arrayChildren[i].px,2)+Math.pow(mouseY-arrayChildren[i].py,2));
+			for (i in arrayChildren) {
+				arrayChildren[i].draw();
 
-			if(distance < arrayChildren[i].r/2){
-				
-				for (var u = 0; u < 4; u++) {
+				distance = Math.sqrt(Math.pow(mouseX-arrayChildren[i].px,2)+Math.pow(mouseY-arrayChildren[i].py,2));
 
-					var xm = (u==0 || u==2) ? 1 : 3;
-					var ym = (u==0 || u==1) ? 1 : 3;
+				if(distance < arrayChildren[i].r/2){
+					
+					for (var u = 0; u < 4; u++) {
 
-					var x = (((arrayChildren[i].s*2)/4)*xm) + (arrayChildren[i].px - arrayChildren[i].s);
-					var y = (((arrayChildren[i].s*2)/4)*ym) + (arrayChildren[i].py - arrayChildren[i].s);
-					var s = ((arrayChildren[i].s*2)/4);
+						var xm = (u==0 || u==2) ? 1 : 3;
+						var ym = (u==0 || u==1) ? 1 : 3;
 
-					child = new ClassCircle.circle({p: { x:x , y:y  },s: s});
-					arrayChildren.push(child);
+						var x = (((arrayChildren[i].s*2)/4)*xm) + (arrayChildren[i].px - arrayChildren[i].s);
+						var y = (((arrayChildren[i].s*2)/4)*ym) + (arrayChildren[i].py - arrayChildren[i].s);
+						var s = ((arrayChildren[i].s*2)/4);
 
-				};
-				arrayChildren[i].destroy();
-				arrayChildren.splice(i,1);
-			}
-		};
+						child = new ClassCircle.circle({p: { x:x , y:y  },s: s});
+						arrayChildren.push(child);
+
+					};
+					arrayChildren[i].destroy();
+					arrayChildren.splice(i,1);
+				}
+			};
+
+			draw();
+
+	  	}, 1000/25 );
 
 	}
-
-	var interval = setInterval( loop, 1000/25 );
 
 })();
 
@@ -74,15 +95,13 @@ function Circle() {
 		this.py = args.p.y;
 		this.s = args.s;
 		this.r = this.s*2;
-		this.l = base/(this.s*2);
-		this.c = color();
 
 	}
 	this.circle.prototype.draw = function(){
 
 		var e = this;
 		ctx.beginPath();
-		ctx.fillStyle = e.c;
+		ctx.fillStyle = color(this.px,this.py);
 		ctx.arc(e.px, e.py, e.s, 0, Math.PI*2, true);
 		ctx.fill();
 	}
@@ -92,13 +111,28 @@ function Circle() {
 		for (key in e) { e[key]=null; }
 	}
 
-	color = function() {
+	color = function(x,y) {
 
-		return 'rgba(' + Math.floor(Math.random() * 255) + ', ' +Math.floor(Math.random() * 255)+', ' + Math.floor(Math.random() * 255) +', 1 )';
+		data = ctx2.getImageData(x, y, 1, 1).data;
+		return 'rgba(' + data[0] + ', ' + data[1] +', ' + data[2] +', ' + data[3] + ' )';
 	}
 }
 
 document.addEventListener('mousemove', function(event){
-	mouseX = event.clientX - (window.innerWidth - _width) * .5;
-	mouseY = event.clientY - (window.innerHeight - _height) * .5;
+	mouseX = event.clientX - (window.innerWidth - width) * .5;
+	mouseY = event.clientY - (window.innerHeight - height) * .5;
 });
+
+if (navigator.getUserMedia)
+    navigator.getUserMedia(constraints,
+
+        function(stream) {
+
+            window.stream = stream;
+
+            vid.src = URL.createObjectURL(stream);
+            vid.play();
+
+        }, function(error) {log("An error occurred: [CODE " + error.code + "]");}
+    );
+else log("Native web camera streaming is not supported in this browser!");
